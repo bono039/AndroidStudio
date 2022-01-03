4df03654733a23335ec71e165a6d8575bf14bdcb
bono039 <wendyhan03@naver.com>
2022-01-03 16:13:16 +0900

    ~p.153 (完)

--- app/src/main/java/com/example/bmical/MainActivity.kt
+++ app/src/main/java/com/example/bmical/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.bmical
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.preference.PreferenceManager
 import com.example.bmical.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +11,51 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.resultButton.setOnClickListener{
-            val intent = Intent(this, ResultActivity::class.java)
-            startActivity(intent)
+        loadData()
+
+        binding.resultButton.setOnClickListener {
+            if (binding.weightEditText.text.isNotBlank() && binding.heightEditText.text.isNotBlank()) {
+
+                // 마지막에 입력한 내용 저장
+                saveData(
+                    binding.heightEditText.text.toString().toFloat(),
+                    binding.weightEditText.text.toString().toFloat()
+                )
+
+                val intent = Intent(this, ResultActivity::class.java).apply {
+                    putExtra(
+                        "weight",
+                        binding.weightEditText.text.toString().toFloat()
+                    ) // 입력받은 문자열을 Float형으로 변경해 인텐트에 데이터 저장
+                    putExtra("height", binding.heightEditText.text.toString().toFloat())
+                }
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun saveData(height: Float, weight: Float) {    // 데이터 저장 메서드
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)  // 객체 얻기
+        val editor = pref.edit()                                // 에디터 객체 얻기 (프리퍼런스에 데이터 얻음)
+
+        editor.putFloat("KEY_HEIGHT", height)   // 쌍으로 키-값 형태 저장
+            .putFloat("KEY_WEIGHT", weight)
+            .apply()                            // 설정값 반영
+    }
+
+    private fun loadData() {
+        val pref = androidx.preference.PreferenceManager.getDefaultSharedPreferences(this) // 프리퍼런스 객체 얻기
+        val height = pref.getFloat("KEY_HEIGHT", 0f)    // 저장된 값 불러오기
+        val weight = pref.getFloat("KEY_WEIGHT", 0f)
+
+        if(height != 0f && weight != 0f) {
+            binding.heightEditText.setText(height.toString())
+            binding.weightEditText.setText(weight.toString())
         }
     }
 }
--- app/src/main/java/com/example/bmical/ResultActivity.kt
+++ app/src/main/java/com/example/bmical/ResultActivity.kt
@@ -2,10 +2,44 @@ package com.example.bmical
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.bmical.databinding.ActivityResultBinding
+import kotlin.math.pow
 
 class ResultActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityResultBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        setContentView(binding.root)    // binding 객체의 root 옮겨받기
+
+        // 전달받은 키 & 몸무게
+        val height = intent.getFloatExtra("height", 0f) // 인텐트에서 데이터 꺼내기
+        val weight = intent.getFloatExtra("weight", 0f)
+
+        // BMI 계산
+        val bmi = weight / (height / 100.0f).pow(2.0f)
+
+        // 결과 표시
+        when {
+            bmi >= 35 -> binding.resultTextView.text = "고도 비만"
+            bmi >= 30 -> binding.resultTextView.text = "2단계 비만"
+            bmi >= 25 -> binding.resultTextView.text = "1단계 비만"
+            bmi >= 23 -> binding.resultTextView.text = "과체중"
+            bmi >= 18.5 -> binding.resultTextView.text = "정상"
+            else -> binding.resultTextView.text = "저체중"
+        }
+
+        // 이미지 표시
+        when {
+            bmi>=23 -> binding.imageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
+            bmi>=18.5 -> binding.imageView.setImageResource(R.drawable.ic_baseline_sentiment_satisfied_alt_24)
+            else -> binding.imageView.setImageResource(R.drawable.ic_baseline_sentiment_dissatisfied_24)
+        }
+
+        Toast.makeText(this, "$bmi", Toast.LENGTH_SHORT).show() // 토스트 메시지 내보내기
+
     }
 }
